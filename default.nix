7bdfd7b201c35b96f02146469288e69bc49e7330
Saikat Das <saikatdas0790@gmail.com>
2023-10-24 21:20:36 +0530

    init: bootstrap project

---
+++ default.nix
@@ -0,0 +1,13 @@
+{}:
+let
+  rev = "51d906d2341c9e866e48c2efcaac0f2d70bfd43e";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  pkgs = import nixpkgs { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    cargo-leptos
+    rustup
+    sass
+  ];
+}
