62eee29dce1779647cde8342cde61c2b1fe3388c
Saikat Das <saikatdas0790@gmail.com>
2023-11-04 12:26:34 +0530

    chore: setup dev server script

--- src/component/some_component.rs
+++ src/component/some_component.rs
@@ -3,11 +3,11 @@ use stylers::style;
 
 #[component]
 pub fn SomeComponent() -> impl IntoView {
-    // let styles = style_sheet!("./style.css");
     let styles = style! {
         h2 {
             color: var(--blue-6);
-            background-color: var(--green-6);
+            background-color: var(--yellow-6);
+            padding: var(--size-7);
         }
     };
 
