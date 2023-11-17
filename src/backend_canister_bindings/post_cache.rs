dd4e3b712b5f37a118890fc725162eabfd0dd48a
Saikat Das <saikatdas0790@gmail.com>
2023-11-17 10:37:55 +0530

    chore: generate rs-agent bindings

--- src/backend_canister_bindings/post_cache.rs
+++ src/backend_canister_bindings/post_cache.rs
@@ -2,7 +2,7 @@
 // You may want to manually adjust some of the types.
 #![allow(dead_code, unused_imports)]
 use candid::{self, CandidType, Deserialize, Principal, Encode, Decode};
-use ic_cdk::api::call::CallResult as Result;
+type Result<T> = std::result::Result<T, ic_agent::AgentError>;
 
 #[derive(CandidType, Deserialize)]
 pub enum KnownPrincipalType {
@@ -40,50 +40,54 @@ pub enum TopPostsFetchError {
 #[derive(CandidType, Deserialize)]
 pub enum Result_ { Ok(Vec<PostScoreIndexItem>), Err(TopPostsFetchError) }
 
-pub struct Service(pub Principal);
-impl Service {
+pub struct Service<'a>(pub Principal, pub &'a ic_agent::Agent);
+impl<'a> Service<'a> {
   pub async fn get_top_posts_aggregated_from_canisters_on_this_network_for_home_feed(
     &self,
     arg0: u64,
     arg1: u64,
-  ) -> Result<(Result_,)> {
-    ic_cdk::call(self.0, "get_top_posts_aggregated_from_canisters_on_this_network_for_home_feed", (
-      arg0,arg1,
-    )).await
+  ) -> Result<Result_> {
+    let args = Encode!(&arg0, &arg1)?;
+    let bytes = self.1.query(&self.0, "get_top_posts_aggregated_from_canisters_on_this_network_for_home_feed").with_arg(args).call().await?;
+    Ok(Decode!(&bytes, Result_)?)
   }
   pub async fn get_top_posts_aggregated_from_canisters_on_this_network_for_hot_or_not_feed(
     &self,
     arg0: u64,
     arg1: u64,
-  ) -> Result<(Result_,)> {
-    ic_cdk::call(self.0, "get_top_posts_aggregated_from_canisters_on_this_network_for_hot_or_not_feed", (
-      arg0,arg1,
-    )).await
+  ) -> Result<Result_> {
+    let args = Encode!(&arg0, &arg1)?;
+    let bytes = self.1.query(&self.0, "get_top_posts_aggregated_from_canisters_on_this_network_for_hot_or_not_feed").with_arg(args).call().await?;
+    Ok(Decode!(&bytes, Result_)?)
   }
   pub async fn get_well_known_principal_value(
     &self,
     arg0: KnownPrincipalType,
-  ) -> Result<(Option<Principal>,)> {
-    ic_cdk::call(self.0, "get_well_known_principal_value", (arg0,)).await
+  ) -> Result<Option<Principal>> {
+    let args = Encode!(&arg0)?;
+    let bytes = self.1.query(&self.0, "get_well_known_principal_value").with_arg(args).call().await?;
+    Ok(Decode!(&bytes, Option<Principal>)?)
   }
   pub async fn receive_top_home_feed_posts_from_publishing_canister(
     &self,
     arg0: Vec<PostScoreIndexItem>,
   ) -> Result<()> {
-    ic_cdk::call(self.0, "receive_top_home_feed_posts_from_publishing_canister", (
-      arg0,
-    )).await
+    let args = Encode!(&arg0)?;
+    let bytes = self.1.update(&self.0, "receive_top_home_feed_posts_from_publishing_canister").with_arg(args).call_and_wait().await?;
+    Ok(Decode!(&bytes)?)
   }
   pub async fn receive_top_hot_or_not_feed_posts_from_publishing_canister(
     &self,
     arg0: Vec<PostScoreIndexItem>,
   ) -> Result<()> {
-    ic_cdk::call(self.0, "receive_top_hot_or_not_feed_posts_from_publishing_canister", (
-      arg0,
-    )).await
+    let args = Encode!(&arg0)?;
+    let bytes = self.1.update(&self.0, "receive_top_hot_or_not_feed_posts_from_publishing_canister").with_arg(args).call_and_wait().await?;
+    Ok(Decode!(&bytes)?)
   }
   pub async fn remove_all_feed_entries(&self) -> Result<()> {
-    ic_cdk::call(self.0, "remove_all_feed_entries", ()).await
+    let args = Encode!()?;
+    let bytes = self.1.update(&self.0, "remove_all_feed_entries").with_arg(args).call_and_wait().await?;
+    Ok(Decode!(&bytes)?)
   }
 }
 
