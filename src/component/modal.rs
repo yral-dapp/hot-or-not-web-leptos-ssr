e532a26661bdb7ff2d3cf09ab157c3b3a748b687
rupansh <rupanshsekar@hotmail.com>
2024-03-12 14:56:11 +0530

    fix: reactive improvements for modal

--- src/component/modal.rs
+++ src/component/modal.rs
@@ -22,16 +22,16 @@ pub fn Modal(#[prop(into)] show: RwSignal<bool>, children: Children) -> impl Int
             class="cursor-pointer modal-bg w-dvw h-dvh absolute left-0 top-0 bg-black/60 z-[99] justify-center items-center"
             style:display=move || if show() { "flex" } else { "none" }
         >
-            <div class="w-full mx-4 pb-8 lg:w-2/5 lg:h-2/5 items-center cursor-auto flex-col flex justify-around bg-neutral-900 rounded-md divide-y-2 divide-neutral-800">
-                <div class="flex w-full justify-end items-center py-4 px-3 md:px-5">
+            <div class="mx-4 py-4 px-8 max-w-full max-h-full items-center cursor-auto flex-col flex justify-around bg-neutral-900 rounded-md divide-y-2 divide-neutral-800">
+                <div class="flex w-full justify-end items-center p-2">
                     <button
                         on:click=move |_| show.set(false)
-                        class="text-white text-center p-1 text-lg md:text-xl bg-orange-600  rounded-full"
+                        class="text-white text-center p-1 text-lg md:text-xl bg-orange-600 rounded-full"
                     >
                         <Icon icon=icondata::ChCross/>
                     </button>
                 </div>
-                {children()}
+                <div class="py-4 w-full">{children()}</div>
             </div>
         </div>
     }
