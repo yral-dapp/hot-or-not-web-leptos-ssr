2fcf3674b055e8d649e4e6a7ed5ffbbef8c5cddc
komal-rs <komalsai@gobazzinga.io>
2024-03-19 19:36:58 +0530

    mobile screen profile width

--- src/page/profile/mod.rs
+++ src/page/profile/mod.rs
@@ -61,7 +61,7 @@ fn ListSwitcher(user_canister: Principal) -> impl IntoView {
                 <Icon icon=icondata::BsTrophy/>
             </button>
         </div>
-        <div class="flex flex-col gap-y-12 justify-center pb-12 w-7/12">
+        <div class="flex flex-col gap-y-12 justify-center pb-12 w-11/12 sm:w-7/12">
             <Show
                 when=move || current_tab() == 0
                 fallback=move || view! { <ProfileSpeculationsPlaceHolder/> }
@@ -87,7 +87,7 @@ fn ProfileViewInner(user: ProfileDetails, user_canister: Principal) -> impl Into
             </div>
             <div class="grid grid-cols-1 gap-5 justify-normal justify-items-center w-full">
                 <div
-                    class="flex flex-row w-7/12"
+                    class="flex flex-row w-11/12 sm:w-7/12"
                     class:justify-center=is_connected()
                     class:justify-between=!is_connected()
                 >
@@ -112,7 +112,7 @@ fn ProfileViewInner(user: ProfileDetails, user_canister: Principal) -> impl Into
                         </div>
                     </Show>
                 </div>
-                <div class="flex justify-around text-center rounded-full divide-x-2 divide-white/20 bg-white/10 p-4 my-4 w-7/12">
+                <div class="flex justify-around text-center rounded-full divide-x-2 divide-white/20 bg-white/10 p-4 my-4 w-11/12 sm:w-7/12">
                     <Stat stat=user.followers_cnt info="Lovers"/>
                     <Stat stat=user.following_cnt info="Loving"/>
                     <Stat stat=user.hots info="Hots"/>
