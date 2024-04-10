cb86ceb6490196cfb6a31059fd215a5c1c6fc479
shreybaz <shreyansh@gobazzinga.io>
2024-04-10 14:22:42 +0530

    added lighthouserc.js file and lighthouse_pipeline.

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+        collect: {
+        url: ['http://127.0.0.1:3000/'],
+        },
+        upload: {
+        target: 'temporary-public-storage',
+        },
+    },
+};
