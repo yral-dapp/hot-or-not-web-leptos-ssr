d82f7662438bc4e67a1a828232837a4148e1ae85
shreybaz <149352258+shreybaz@users.noreply.github.com>
2024-04-16 15:05:51 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,11 @@ module.exports = {
         url: ['https://yral.com/'],
         },
         upload: {
-        target: 'temporary-public-storage',
+            target: 'temporary-public-storage',
+        },
+        assert: {
+            "categories:performance": ["warn", {"minScore": 0.3}],
+            "categories:accessibility": ["warn", {"minScore": 0.5}]
         },
     },
 };
