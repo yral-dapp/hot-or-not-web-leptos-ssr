9e7a3b6b10935f42a1ee359c2bb789094ad9e507
komal-rs <komalsai@gobazzinga.io>
2024-04-16 22:22:37 +0530

    minor fix

--- src/utils/event_streaming.rs
+++ src/utils/event_streaming.rs
@@ -72,7 +72,9 @@ pub async fn stream_to_offchain_agent(event: String, params: String) -> Result<(
     let off_chain_agent_url = OFF_CHAIN_AGENT_GRPC_URL.as_ref();
     let channel = Channel::from_static(off_chain_agent_url).connect().await?;
 
-    let off_chain_agent_grpc_auth_token = env::var("GRPC_AUTH_TOKEN").expect("GRPC_AUTH_TOKEN");
+    let mut off_chain_agent_grpc_auth_token = env::var("GRPC_AUTH_TOKEN").expect("GRPC_AUTH_TOKEN");
+    // removing whitespaces and new lines for proper parsing
+    off_chain_agent_grpc_auth_token.retain(|c| !c.is_whitespace());
 
     let token: MetadataValue<_> = format!("Bearer {}", off_chain_agent_grpc_auth_token).parse()?;
 
