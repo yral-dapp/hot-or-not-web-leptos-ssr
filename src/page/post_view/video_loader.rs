c3ff85838ff65c6fa271497d079f30608b1dfa6f
rupansh <rupanshsekar@hotmail.com>
2024-04-17 22:50:27 +0530

    fix(post_view): remove unnecessary Suspense block
    fixes various hydation bugs for initial video load

--- src/page/post_view/video_loader.rs
+++ src/page/post_view/video_loader.rs
@@ -342,27 +342,25 @@ pub fn VideoView(idx: usize, muted: RwSignal<bool>) -> impl IntoView {
     }
 
     view! {
-        <Suspense>
-            <label class="w-full h-full absolute top-0 left-0 grid grid-cols-1 justify-items-center items-center cursor-pointer z-[3]">
-                <input
-                    on:change=move |_| muted.update(|m| *m = !*m)
-                    type="checkbox"
-                    value=""
-                    class="sr-only"
-                />
-                <video
-                    _ref=container_ref
-                    class="object-contain h-dvh max-h-dvh cursor-pointer"
-                    poster=view_bg_url
-                    src=view_video_url
-                    loop
-                    muted
-                    playsinline
-                    disablepictureinpicture
-                    disableremoteplayback
-                    preload="auto"
-                ></video>
-            </label>
-        </Suspense>
+        <label class="w-full h-full absolute top-0 left-0 grid grid-cols-1 justify-items-center items-center cursor-pointer z-[3]">
+            <input
+                on:change=move |_| muted.update(|m| *m = !*m)
+                type="checkbox"
+                value=""
+                class="sr-only"
+            />
+            <video
+                _ref=container_ref
+                class="object-contain h-dvh max-h-dvh cursor-pointer"
+                poster=view_bg_url
+                src=view_video_url
+                loop
+                muted
+                playsinline
+                disablepictureinpicture
+                disableremoteplayback
+                preload="auto"
+            ></video>
+        </label>
     }
 }
