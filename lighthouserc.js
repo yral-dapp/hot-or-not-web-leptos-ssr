e56eefed6496faea2ba5a1697612f4a29eb3eb8e
shreybaz <149352258+shreybaz@users.noreply.github.com>
2024-04-22 13:08:32 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-        url: ['http://127.0.0.1:3000'],
+        url: ['https://yral.com/'],
         },
         upload: {
             target: 'temporary-public-storage',
