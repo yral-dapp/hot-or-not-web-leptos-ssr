e2d798d2815308f7c7fb7789ba1c4f709cc67b88
shreybaz <shreyansh@gobazzinga.io>
2024-04-30 00:16:34 +0530

    added like test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,6 @@
 import { chromium } from 'playwright';
 import { test, expect } from '@playwright/test';
 import nock from 'nock';
-import { assert } from 'console';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://yral.com/');
@@ -93,8 +92,6 @@ test.describe('New test', () => {
     const third_duration = await third_video.evaluate(new_video => new_video.duration);
     console.log('3rd Video Duration:', third_duration);
 
-    // await page.waitForTimeout(3000);
-
   });
 
   test('Test google login', async ({page}) => {
@@ -114,7 +111,7 @@ test.describe('New test', () => {
       await page.getByRole('button', { name: 'Google Sign-In' }).click();
       const page1 = await page1Promise;
       await page1.getByLabel('Email or phone').click();
-      await page1.getByLabel('Email or phone').fill('testautomationyral@gmail.com');
+      await page1.getByLabel('Email or phone').fill('testautomationyral@googlemail.com');
       
       await page.waitForTimeout(2000);
       await page1.getByLabel('Email or phone').press('Enter');
@@ -143,7 +140,6 @@ test.describe('New test', () => {
       console.error("error in login"); 
     }
   });
-
 
   // test('TEST UPLOAD FLOW', async ({page}) => {
 
@@ -201,48 +197,42 @@ test.describe('New test', () => {
 
   // });
 
-  //test like functionality
-  test('Test like', async ({page}) => {
+  // //test like functionality
+  // test('Test like', async ({page}) => {
 
-    // login from wallet: using headfull mode
-    // try {
-      console.log('test like');
+  //   // login from wallet: using headfull mode
+  //   // try {
+  //     console.log('test like');
 
-      await page.waitForTimeout(1000);
+  //     await page.waitForTimeout(1000);
 
-      let likes_locator = page.locator('.flex > div:nth-child(2) > .flex > .text-sm').first();
-      let total_likes = await likes_locator.textContent();
-      console.log("total likes: ", total_likes );
+  //     let likes_locator = page.locator('.flex > div:nth-child(2) > .flex > .text-sm').first();
+  //     let total_likes = await likes_locator.textContent();
+  //     console.log("total likes: ", total_likes );
 
-      await page.waitForTimeout(15000);
+  //     await page.waitForTimeout(15000);
 
-      await expect(page.locator('button').first()).toBeVisible();
-      console.log('like button visible');
+  //     await expect(page.locator('button').first()).toBeVisible();
+  //     console.log('like button visible');
 
-      //click like button
-      await page.locator('button').first().click();
-      // await page.locator('body > main > div.h-full.w-full.overflow-hidden.overflow-y-auto > div > div:nth-child(2) > div > div.flex.flex-row.flex-nowrap.justify-between.items-end.pb-16.px-2.md\:px-6.w-full.text-white.absolute.bottom-0.left-0.bg-transparent.z-\[4\] > div.flex.flex-col.gap-6.items-end.w-3\/12.text-4xl > div > button > svg > path').click();
-      await page.waitForTimeout(2000);
+  //     //click like button
+  //     await page.locator('button').first().click();
+  //     // await page.locator('body > main > div.h-full.w-full.overflow-hidden.overflow-y-auto > div > div:nth-child(2) > div > div.flex.flex-row.flex-nowrap.justify-between.items-end.pb-16.px-2.md\:px-6.w-full.text-white.absolute.bottom-0.left-0.bg-transparent.z-\[4\] > div.flex.flex-col.gap-6.items-end.w-3\/12.text-4xl > div > button > svg > path').click();
+  //     await page.waitForTimeout(2000);
 
-      let new_likes_locator = page.locator('.flex > div:nth-child(2) > .flex > .text-sm').first();
-      let updated_likes = await new_likes_locator.textContent();
-      console.log("updated total likes: ", updated_likes );
+  //     let new_likes_locator = page.locator('.flex > div:nth-child(2) > .flex > .text-sm').first();
+  //     let updated_likes = await new_likes_locator.textContent();
+  //     console.log("updated total likes: ", updated_likes );
 
-      expect(parseInt(updated_likes)).toBe(parseInt(total_likes) + 1);
-      // let new_like_count = expect(page.getByRole('main'));
-      // console.log("new like count: ", new_like_count );
+  //     expect(parseInt(updated_likes)).toBe(parseInt(total_likes) + 1);
+  //     // let new_like_count = expect(page.getByRole('main'));
+  //     // console.log("new like count: ", new_like_count );
 
-      // await expect(page.getByRole('main')).toContainText(like_count + 1);
+  //     // await expect(page.getByRole('main')).toContainText(like_count + 1);
 
-    // } catch (error) {
-    //   console.error("error in login");
-    // }
-  });
+  //   // } catch (error) {
+  //   //   console.error("error in login");
+  //   // }
+  // });
 
 });
-
-
-
-
-// upload 
-//       getByText('UploadUploadedProcessedPublished Continue Browsing')
