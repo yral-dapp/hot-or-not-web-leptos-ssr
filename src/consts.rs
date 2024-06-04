d3528b6aa3c6be5399f6ad55771ec0cdd5e25e16
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-06-04 23:30:23 +0530

    feat: update webhook status which can be changed later
    false - means taht webhook is not received yet
    
    true - (set in off-chain-agent) means that webhook is received

--- src/consts.rs
+++ src/consts.rs
@@ -2,6 +2,16 @@ use candid::Principal;
 use once_cell::sync::Lazy;
 use reqwest::Url;
 
+#[macro_export]
+macro_rules! env_or_default {
+    ($env_var:expr, $default:expr) => {{
+        std::env::var($env_var)
+            .ok()
+            .map(|val| val)
+            .unwrap_or_else(|| $default.into())
+    }};
+}
+
 pub const CF_STREAM_BASE: &str = "https://customer-2p3jflss4r4hmpnz.cloudflarestream.com";
 pub const FALLBACK_PROPIC_BASE: &str = "https://api.dicebear.com/7.x/big-smile/svg";
 pub const CF_WATERMARK_UID: &str = "c094ef579b950a6a5ae3e482268b81ca";
@@ -16,8 +26,14 @@ pub static METADATA_API_BASE: Lazy<Url> =
     Lazy::new(|| Url::parse("https://yral-metadata.fly.dev").unwrap());
 pub static OFF_CHAIN_AGENT_GRPC_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://icp-off-chain-agent.fly.dev:443").unwrap());
-// G-6W5Q2MRX0E to test locally | G-PLNNETMSLM
-pub static GTAG_MEASUREMENT_ID: Lazy<&str> = Lazy::new(|| "G-PLNNETMSLM");
+// // G-6W5Q2MRX0E to test locally | G-PLNNETMSLM
+// pub static GTAG_MEASUREMENT_ID: Lazy<&str> = Lazy::new(|| "G-PLNNETMSLM");
+
+const DEFAULT_GTAG_MEASUREMENT_ID: &str = "G-PLNNETMSLM";
+
+pub static GTAG_MEASUREMENT_ID: Lazy<String> = Lazy::new(|| {
+    env_or_default!("YRAL_GTAG_MEASUREMENT_ID", DEFAULT_GTAG_MEASUREMENT_ID).to_string()
+});
 
 pub mod social {
     pub const TELEGRAM: &str = "https://t.me/+c-LTX0Cp-ENmMzI1";
@@ -42,3 +58,5 @@ pub mod google {
     pub const GOOGLE_TOKEN_URL: &str = "https://oauth2.googleapis.com/token";
     pub const GOOGLE_ISSUER_URL: &str = "https://accounts.google.com";
 }
+
+
