60631422c999240e87f28311268387a4e7d72aa5
komal-rs <komalsai@gobazzinga.io>
2024-08-27 18:10:08 +0530

    event segregation

--- ssr/src/utils/event_streaming/mod.rs
+++ ssr/src/utils/event_streaming/mod.rs
@@ -66,9 +66,11 @@ pub fn send_event_warehouse(event_name: &str, params: &serde_json::Value) {
     let event_name = event_name.to_string();
     let params_str = params.to_string();
 
-    let host_str = get_host();
-    let mut params = params.clone();
-    params["host"] = json!(host_str);
+    if params["host"].is_null() {
+        let host_str = get_host();
+        let mut params = params.clone();
+        params["host"] = json!(host_str);
+    }
 
     spawn_local(async move {
         stream_to_offchain_agent(event_name, params_str)
