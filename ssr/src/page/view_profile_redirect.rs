7341193520ca53036c3172f9eb4487758a09a908
veercodeprog <Veerpratap945050@gmail.com>
2024-08-30 16:09:57 +0530

    Add profile icon with working link

--- ssr/src/page/view_profile_redirect.rs
+++ ssr/src/page/view_profile_redirect.rs
@@ -1,18 +1,6 @@
 use crate::component::{canisters_prov::AuthCansProvider, spinner::FullScreenSpinner};
 use leptos::*;
 use leptos_router::Redirect;
-
-#[component]
-fn ProfileLoading() -> impl IntoView {
-    view! {
-        <div class="rounded-full animate-pulse basis-4/12 aspect-square overflow-clip bg-white/20"></div>
-        <div class="flex flex-col gap-2 animate-pulse basis-8/12">
-            <div class="w-full h-4 rounded-full bg-white/20"></div>
-            <div class="w-full h-4 rounded-full bg-white/20"></div>
-        </div>
-    }
-}
-
 #[component]
 pub fn ProfileInfo() -> impl IntoView {
     view! {
