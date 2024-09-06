f7938e5defb0aacde0e673fd9092773704d20902
abhishek <abhishektripathi@gobazzinga.io>
2024-09-06 15:40:55 +0530

    fix: default stub message for error template

--- ssr/src/error_template.rs
+++ ssr/src/error_template.rs
@@ -51,6 +51,12 @@ pub fn ErrorTemplate(
         .collect();
     println!("Errors: {errors:#?}");
 
+    let error_string = if !errors.is_empty() {
+        "It looks like our system is taking a coffee break. Try again in a bit, and we'll have it back to work!".to_string()
+    } else {
+        String::new()
+    };
+
     // Only the response code for the first error is actually sent from the server
     // this may be customized by the specific application
     #[cfg(feature = "ssr")]
@@ -66,23 +72,7 @@ pub fn ErrorTemplate(
             <img src="/img/error-logo.svg"/>
             <h1 class="p-2 text-2xl md:text-3xl font-bold text-white">"oh no!"</h1>
             <div class="text-center text-xs md:text-sm text-white/60 w-full md:w-2/3 lg:w-1/3 resize-none px-8 mb-4">
-                // <h1>{if errors.len() > 1 { "Errors" } else { "Error" }}</h1>
-                <For
-                    // a function that returns the items we're iterating over; a signal is fine
-                    each=move || { errors.clone().into_iter().enumerate() }
-                    // a unique key for each item as a reference
-                    key=|(index, _error)| *index
-                    // renders each item to a view
-                    children=move |error| {
-                        let error_string = error.1.to_string();
-                        let error_code = error.1.status_code();
-                        view! {
-                            <h2>{error_code.to_string()}</h2>
-                            <p>"Error: " {error_string}</p>
-                        }
-                    }
-                />
-
+                {error_string.clone()}
             </div>
             <button
                 on:click=move |_| go_back()
