2304868dc54ff38cb8b0ad133abbfaef2b045c51
debjit <debjitbhowal.db@gmail.com>
2024-09-10 00:34:54 +0530

    candid files and dependencies

--- ssr/src/consts/local.rs
+++ ssr/src/consts/local.rs
@@ -8,3 +8,5 @@ pub const AGENT_URL: &str = "http://localhost:4943";
 
 pub const YRAL_BACKEND_CONTAINER_TAG: &str = "76bfd0fa78e4f862a4b30601f4ff3143aa974ee7";
 pub const YRAL_METADATA_CONTAINER_TAG: &str = "a4879e2e711c17beeb12ed6987ba315c110be9e5";
+
+pub const ICP_LEDGER_CANISTER_ID: &'static str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
