269418a5c9ccb2f82e0456077aab403cc3ab88e2
veer-asbytes <veerpratap@gobazzinga.io>
2024-09-20 01:18:52 +0530

    local changes

--- ssr/src/page/wallet/tokens.rs
+++ ssr/src/page/wallet/tokens.rs
@@ -1,6 +1,9 @@
 use candid::Principal;
 use ic_agent::AgentError;
+use leptos_use::use_window;
 
+use crate::page::wallet::ShareProfilePopup;
+use crate::utils::web::{check_share_support, share_url};
 use crate::{
     canister::individual_user_template::Result14,
     component::{
@@ -78,7 +81,7 @@ async fn token_metadata_or_fallback(
 #[component]
 fn FallbackToken() -> impl IntoView {
     view! {
-        <div class="w-full items-center h-20 rounded-xl border-2 border-neutral-700 bg-white/15 animate-pulse"></div>
+        <div class="items-center w-full h-20 rounded-xl border-2 animate-pulse border-neutral-700 bg-white/15"></div>
     }
 }
 
@@ -96,27 +99,57 @@ pub fn TokenView(
     );
 
     view! {
-        <ClaimTokensOrRedirectError token_root/>
+        <ClaimTokensOrRedirectError token_root />
         <Suspense fallback=FallbackToken>
             {move || {
                 info.map(|info| {
+                    let base_url = || {
+                        use_window().as_ref().and_then(|w| w.location().origin().ok())
+                    };
+                    let username_or_principal = user_principal.to_text().clone();
+                    let principal = user_principal.to_text().clone();
+                    let share_link = base_url()
+                        .map(|b| format!("{b}/profile/{}?tab=tokens", &username_or_principal))
+                        .unwrap_or_default();
+                    let message = format!(
+                        "Hey! Check out my YRAL profile 👇 {}. I just minted my own token—come see and create yours! 🚀 #YRAL #TokenMinter",
+                        share_link.clone(),
+                    );
+                    let share_action = create_action(move |&()| async move { Ok(()) });
+                    let link = share_link.clone();
+                    let share_profile_url = move || {
+                        let has_share_support = check_share_support();
+                        match has_share_support {
+                            Some(_) => {
+                                share_url(&link);
+                            }
+                            None => {
+                                share_action.dispatch(());
+                            }
+                        };
+                    };
                     view! {
                         <a
-                            href=format!("/token/info/{token_root}/{user_principal}")
+                            href=format!("/token/info/{token_root}/{principal}")
                             _ref=_ref
-                            class="grid grid-cols-2 grid-rows-1 w-full items-center p-4 rounded-xl border-2 border-neutral-700 bg-white/15"
+                            class="grid grid-cols-2 grid-rows-1 items-center p-4 w-full rounded-xl border-2 border-neutral-700 bg-white/15"
                         >
-                            <div class="flex flex-row gap-2 items-center justify-self-start">
-                                <img class="w-12 h-12 rounded-full" src=info.logo_b64.clone()/>
+                            <div class="flex flex-row gap-2 justify-self-start items-center">
+                                <img class="w-12 h-12 rounded-full" src=info.logo_b64.clone() />
                                 <span class="text-white truncate">{info.name.clone()}</span>
                             </div>
-                            <div class="flex flex-row gap-2 items-center justify-self-end text-base text-white">
+                            <div class="flex flex-row gap-2 justify-self-end items-center text-base text-white">
                                 <span class="truncate">
                                     {format!("{} {}", info.balance.humanize(), info.symbol)}
                                 </span>
-                                <div class="flex items-center justify-center w-8 h-8 bg-white/15 rounded-full">
-                                    <Icon icon=icondata::BsSend/>
-                                </div>
+                                <button
+                                    on:click=move |_| share_profile_url()
+                                    class="p-1 text-lg text-center text-white rounded-full md:text-xl bg-primary-600"
+                                >
+                                    <Icon icon=icondata::AiShareAltOutlined />
+
+                                </button>
+                                <ShareProfilePopup sharing_action=share_action share_link message />
                             </div>
                         </a>
                     }
@@ -134,12 +167,12 @@ fn TokenList(canisters: Canisters<true>) -> impl IntoView {
     let provider: TokenRootList = TokenRootList(canisters);
 
     view! {
-        <div class="flex flex-col w-full gap-2 items-center">
+        <div class="flex flex-col gap-2 items-center w-full">
             <InfiniteScroller
                 provider
                 fetch_count=10
                 children=move |token_root, _ref| {
-                    view! { <TokenView user_principal token_root _ref=_ref.unwrap_or_default()/> }
+                    view! { <TokenView user_principal token_root _ref=_ref.unwrap_or_default() /> }
                 }
             />
 
@@ -150,16 +183,16 @@ fn TokenList(canisters: Canisters<true>) -> impl IntoView {
 #[component]
 pub fn Tokens() -> impl IntoView {
     view! {
-        <div class="felx items-center flex-col w-dvw min-h-dvh gap-6 bg-black pt-4 px-4 pb-12">
+        <div class="flex-col gap-6 items-center px-4 pt-4 pb-12 bg-black felx w-dvw min-h-dvh">
             <Title justify_center=false>
                 <div class="flex flex-row justify-between">
-                    <BackButton fallback="/wallet".to_string()/>
-                    <span class="text-xl text-white font-bold">Tokens</span>
+                    <BackButton fallback="/wallet".to_string() />
+                    <span class="text-xl font-bold text-white">Tokens</span>
                     <div></div>
                 </div>
             </Title>
             <AuthCansProvider fallback=BulletLoader let:canisters>
-                <TokenList canisters/>
+                <TokenList canisters />
             </AuthCansProvider>
         </div>
     }
