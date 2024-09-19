2196bb322e6253644eec1169fa6dda8fb5517e0b
komal-rs <komalsai@gobazzinga.io>
2024-09-19 16:15:42 +0530

    host

--- ssr/src/utils/host.rs
+++ ssr/src/utils/host.rs
@@ -20,5 +20,7 @@ pub fn show_cdao_page() -> bool {
     let host = get_host();
     host == "icpump.fun"
         || host == "localhost:3000"
-        || host.contains("go-bazzinga-hot-or-not-web-leptos-ssr.fly.dev")
+        || host == "hot-or-not-web-leptos-ssr-staging.fly.dev"
+    // Use this when testing icpump changes
+    // || host.contains("go-bazzinga-hot-or-not-web-leptos-ssr.fly.dev")
 }
