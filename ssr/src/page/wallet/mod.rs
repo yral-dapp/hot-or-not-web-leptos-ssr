db9900ee4ea6443c29450f2c0677654552a29ee1
veer-asbytes <veerpratap@gobazzinga.io>
2024-09-24 06:25:41 +0530

    Fix wallet page content overflow and remove unnecessary horizontal scroll

--- ssr/src/page/wallet/mod.rs
+++ ssr/src/page/wallet/mod.rs
@@ -35,7 +35,7 @@ fn ProfileGreeter(details: ProfileDetails) -> impl IntoView {
         <div class="flex flex-col">
             <span class="text-white/50 text-md">Welcome!</span>
             <div class="flex flex-row gap-2">
-                <span class="text-white text-lg md:text-xl truncate">
+                <span class="text-lg text-white md:text-xl truncate">
                     // TEMP: Workaround for hydration bug until leptos 0.7
                     // class=("md:w-5/12", move || !is_connected())
                     {details.display_name_or_fallback()}
@@ -44,8 +44,8 @@ fn ProfileGreeter(details: ProfileDetails) -> impl IntoView {
                 <ShareButtonWithFallbackPopup share_link message />
             </div>
         </div>
-        <div class="w-16 aspect-square overflow-clip justify-self-end rounded-full">
-            <img class="h-full w-full object-cover" src=details.profile_pic_or_random()/>
+        <div class="justify-self-end w-16 rounded-full aspect-square overflow-clip">
+            <img class="object-cover w-full h-full" src=details.profile_pic_or_random() />
         </div>
     }
 }
@@ -55,9 +55,9 @@ fn FallbackGreeter() -> impl IntoView {
     view! {
         <div class="flex flex-col">
             <span class="text-white/50 text-md">Welcome!</span>
-            <div class="w-3/4 rounded-full py-2 bg-white/40 animate-pulse"></div>
+            <div class="py-2 w-3/4 rounded-full animate-pulse bg-white/40"></div>
         </div>
-        <div class="w-16 aspect-square overflow-clip rounded-full justify-self-end bg-white/40 animate-pulse"></div>
+        <div class="justify-self-end w-16 rounded-full animate-pulse aspect-square overflow-clip bg-white/40"></div>
     }
 }
 
@@ -65,7 +65,7 @@ const RECENT_TXN_CNT: usize = 10;
 
 #[component]
 fn BalanceFallback() -> impl IntoView {
-    view! { <div class="w-1/4 rounded-full py-3 mt-1 bg-white/30 animate-pulse"></div> }
+    view! { <div class="py-3 mt-1 w-1/4 rounded-full animate-pulse bg-white/30"></div> }
 }
 
 #[component]
@@ -95,7 +95,7 @@ fn TokensFetch() -> impl IntoView {
                             .unwrap_or(Principal::anonymous());
                         view! {
                             <For each=move || tokens.clone() key=|inf| inf.key() let:token_root>
-                                <TokenView user_principal token_root/>
+                                <TokenView user_principal token_root />
                             </For>
                         }
                     })
@@ -133,14 +133,14 @@ pub fn Wallet() -> impl IntoView {
 
     view! {
         <div>
-            <div class="flex flex-col w-dvw min-h-dvh bg-black gap-4 px-4 pt-4 pb-12">
+            <div class="flex flex-col gap-4 px-4 pt-4 pb-12 bg-black min-h-dvh">
                 <div class="grid grid-cols-2 grid-rows-1 items-center w-full">
                     <AuthCansProvider fallback=FallbackGreeter let:cans>
-                        <ProfileGreeter details=cans.profile_details()/>
+                        <ProfileGreeter details=cans.profile_details() />
                     </AuthCansProvider>
                 </div>
-                <div class="flex flex-col w-full items-center mt-6 text-white">
-                    <span class="text-md lg:text-lg uppercase">Your Coyns Balance</span>
+                <div class="flex flex-col items-center mt-6 w-full text-white">
+                    <span class="uppercase lg:text-lg text-md">Your Coyns Balance</span>
                     <Suspense fallback=BalanceFallback>
                         {move || {
                             let balance = try_or_redirect_opt!(balance_fetch() ?);
@@ -150,8 +150,8 @@ pub fn Wallet() -> impl IntoView {
                     </Suspense>
                 </div>
                 <Show when=move || !is_connected()>
-                    <div class="flex flex-col w-full py-5 items-center">
-                        <div class="flex flex-row w-9/12 md:w-5/12 items-center">
+                    <div class="flex flex-col items-center py-5 w-full">
+                        <div class="flex flex-row items-center w-9/12 md:w-5/12">
                             <ConnectLogin
                                 login_text="Login to claim your COYNs"
                                 cta_location="wallet"
@@ -159,21 +159,21 @@ pub fn Wallet() -> impl IntoView {
                         </div>
                     </div>
                 </Show>
-                <div class="flex flex-col w-full gap-2">
-                    <div class="flex flex-row w-full items-end justify-between">
-                        <span class="text-white text-sm md:text-md">My Tokens</span>
-                        <a href="/tokens" class="text-white/50 text-md md:text-lg">
+                <div class="flex flex-col gap-2 w-full">
+                    <div class="flex flex-row justify-between items-end w-full">
+                        <span class="text-sm text-white md:text-md">My Tokens</span>
+                        <a href="/tokens" class="md:text-lg text-white/50 text-md">
                             See All
                         </a>
                     </div>
                     <div class="flex flex-col gap-2 items-center">
-                        <TokensFetch/>
+                        <TokensFetch />
                     </div>
                 </div>
-                <div class="flex flex-col w-full gap-2">
-                    <div class="flex flex-row w-full items-end justify-between">
-                        <span class="text-white text-sm md:text-md">Recent Transactions</span>
-                        <a href="/transactions" class="text-white/50 text-md md:text-lg">
+                <div class="flex flex-col gap-2 w-full">
+                    <div class="flex flex-row justify-between items-end w-full">
+                        <span class="text-sm text-white md:text-md">Recent Transactions</span>
+                        <a href="/transactions" class="md:text-lg text-white/50 text-md">
                             See All
                         </a>
                     </div>
@@ -188,7 +188,7 @@ pub fn Wallet() -> impl IntoView {
                                                 key=|inf| inf.key()
                                                 let:info
                                             >
-                                                <TxnView info/>
+                                                <TxnView info />
                                             </For>
                                         }
                                     })
