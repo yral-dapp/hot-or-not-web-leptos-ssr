6d29fb0110698a927b4b2b6142b2e97568868812
harshbaz <harsh@gobazzinga.io>
2024-09-24 12:29:46 +0530

    Fix spacings

--- ssr/src/page/icpump/mod.rs
+++ ssr/src/page/icpump/mod.rs
@@ -10,20 +10,18 @@ use crate::utils::token::icpump::TokenListItem;
 #[component]
 pub fn TokenListing(details: TokenListItem) -> impl IntoView {
     view! {
-        <a href={details.link}>
-            <div class="relative flex h-fit max-h-[300px] w-full gap-2 overflow-hidden border border-transparent p-2 transition-colors hover:border-gray-700 active:border-gray-200">
-                <div class="min-w-32 relative self-start p-1">
-                <img class="mr-4 w-32 h-auto select-none" src={details.logo} alt={details.token_name.clone()}/>
+        <a href={details.link} class="relative flex h-fit max-h-[300px] w-full gap-2 overflow-hidden border border-transparent p-2 transition-colors hover:border-gray-700 active:border-gray-200">
+            <div class="min-w-32 relative self-start p-1">
+            <img class="mr-4 w-32 h-auto select-none" src={details.logo} alt={details.token_name.clone()}/>
+            </div>
+            <div class="gap-1 flex-col flex h-fit">
+                <div class="flex items-center justify-between gap-4 text-gray-200">
+                    <span class="line-clamp-1 w-full overflow-hidden" style="word-break: break-word;">{details.token_name}</span>
+                    <span class="shrink-0 font-bold underline"><span class="text-gray-400 italic select-none">"$"</span>{details.token_symbol}</span>
                 </div>
-                <div class="gap-1 flex-col flex h-fit">
-                    <div class="flex items-center justify-between gap-4 text-gray-200">
-                        <span class="line-clamp-1 w-full overflow-hidden">{details.token_name}</span>
-                        <span class="shrink-0 font-bold underline"><span class="text-gray-400 italic select-none">"$"</span>{details.token_symbol}</span>
-                    </div>
-                    <div title={details.description.clone()} class="text-sm line-clamp-3 text-gray-400">{details.description}</div>
-                    <div class="text-xs text-gray-500">"Created by: "<span class="select-all">{details.user_id}</span></div>
-                    <span class="absolute bottom-3 right-2 shrink-0 text-xs text-gray-500 underline">{details.formatted_created_at}</span>
-                </div>
+                <div title={details.description.clone()} class="text-sm line-clamp-3 text-gray-400" style="word-break: break-word">{details.description}</div>
+                <div class="text-xs text-gray-500 line-clamp-2 pr-10">"Created by: "<span class="select-all">{details.user_id}</span><span class="invisible">{details.formatted_created_at.clone()}</span></div>
+                <span class="absolute bottom-3 right-2 shrink-0 text-xs text-gray-500 underline">{details.formatted_created_at}</span>
             </div>
         </a>
     }
@@ -46,7 +44,7 @@ pub fn ICPumpListing() -> impl IntoView {
 
     view! {
 
-        <div class="flex flex-col justify-center mt-10 mb-10">
+        <div class="flex flex-col justify-center mt-6 mb-10">
             <Suspense fallback=FullScreenSpinner>
                 {move || {
                     let _ = act.get().map(|res| {
@@ -101,12 +99,12 @@ pub fn ICPumpListing() -> impl IntoView {
 pub fn ICPumpLanding() -> impl IntoView {
     view! {
         <div class="min-h-screen bg-black text-white overflow-y-scroll pt-5 pb-12">
-            <div class="flex ml-4 space-x-2">
+            <div class="flex ml-4 space-x-2 justify-center items-center sm:justify-start">
                 <div class="text-gray-100 hover:text-pink-200 hover:underline active:text-pink-500 active:italic"> <a href="https://twitter.com/Yral_app" target="_blank"> [twitter] </a> </div>
                 <div class="text-gray-100 hover:text-pink-200 hover:underline active:text-pink-500 active:italic"> <a href="https://www.instagram.com/yral_app/" target="_blank"> [instagram] </a> </div>
                 <div class="text-gray-100 hover:text-pink-200 hover:underline active:text-pink-500 active:italic"> <a href="https://t.me/+c-LTX0Cp-ENmMzI1" target="_blank"> [telegram] </a> </div>
             </div>
-            <div class="flex justify-center items-center">
+            <div class="flex justify-center items-center pt-6">
                 <div class="font-bold text-3xl hover:font-extrabold hover:underline active:italic animate-blink-colors"> <a href="/token/create"> [ create a new coin ] </a> </div>
             </div>
             <div class="px-4">
