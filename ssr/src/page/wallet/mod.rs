86c53785672e750e84fff63127cc7e2574ca3997
joel-in-bytes <joel@gobazzinga.io>
2024-10-02 11:40:13 +0530

    removed see all button

--- ssr/src/page/wallet/mod.rs
+++ ssr/src/page/wallet/mod.rs
@@ -242,7 +242,7 @@ pub fn WalletImpl(principal: Principal) -> impl IntoView {
                         move ||{
                             let is_own_account = try_or_redirect_opt!(is_own_account() ?);
                             Some(view! {
-                                <span class="uppercase lg:text-lg text-md">{if is_own_account{"Your Coyns Balance"} else {"Their Coyns Balance"}}</span>
+                                <span class="uppercase lg:text-lg text-md">{if is_own_account{"Your Coyns Balance"} else {"Coyns Balance"}}</span>
                             })
                         }
                     }
@@ -284,10 +284,7 @@ pub fn WalletImpl(principal: Principal) -> impl IntoView {
                             Some(
                                 view! {
                                     <div class="flex flex-row justify-between items-end w-full">
-                                        <span class="text-sm text-white md:text-md">{if is_own_account{"My Tokens"} else {"Their Tokens"}}</span>
-                                        <a href="/tokens" class="md:text-lg text-white/50 text-md">
-                                            See All
-                                        </a>
+                                        <span class="text-sm text-white md:text-md">{if is_own_account{"My Tokens"} else {"Tokens"}}</span>
                                     </div>
                                 }
                             )
@@ -307,9 +304,6 @@ pub fn WalletImpl(principal: Principal) -> impl IntoView {
                 <div class="flex flex-col gap-2 w-full">
                     <div class="flex flex-row justify-between items-end w-full">
                         <span class="text-sm text-white md:text-md">Recent Transactions</span>
-                        <a href="/transactions" class="md:text-lg text-white/50 text-md">
-                            See All
-                        </a>
                     </div>
                     <div class="flex flex-col divide-y divide-white/10">
                         <Suspense fallback=BulletLoader>
