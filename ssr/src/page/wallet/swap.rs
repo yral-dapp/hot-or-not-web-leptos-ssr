8d274fc0df7f8a0f19ccd85b76f57ded190fefd8
harsh-mn-yral <harsh@gobazzinga.io>
2024-12-06 15:58:16 +0530

    Update

--- ssr/src/page/wallet/swap.rs
+++ ssr/src/page/wallet/swap.rs
@@ -1,52 +1,68 @@
+#[derive(Clone)]
 struct SwapToken {
     name: String,
     icon: String,
+    symbol: String,
 }
 
+use html::Div;
 use leptos::*;
+use leptos_use::on_click_outside;
+use rust_decimal::prelude::Signed;
 
 use crate::component::icons::chevron_left_icon::ChevronLeftIcon;
 use crate::component::icons::arrow_down_icon::ArrowDownIcon;
 use crate::component::icons::wallet_icon::WalletIcon;
 use crate::component::icons::chevron_right_icon::ChevronRightIcon;
-use crate::component::button::Button;
+use crate::component::icons::arrow_right_long_icon::ArrowRightLongIcon;
+use crate::component::icons::close_icon::CloseIcon;
+use crate::component::buttons::Button;
 
 #[component]
 pub fn SwapPage() -> impl IntoView {
 
-  let wallet_balance = create_signal(100.0);
+  let (wallet_balance, set_wallet_balance) = create_signal(100.0);
 
   let swap_from = SwapToken {
-    name: "HOT".to_string(),
+    name: "Hot".to_string(),
     icon: "https://picsum.photos/200".to_string(),
+    symbol: "HOT".to_string(),
   };
 
   let profile_image = "https://picsum.photos/200";
   let profile_id = "mqxpy-vp4st-vhw6p-poxzk-i363n-y4fag-v37dn-22pu7";
 
   
-  let from_amount = create_signal(0);
-  let to_amount = create_signal(0);
-  let selected_token = create_signal(None);
-  let selected_token_wallet_balance = create_signal(0);
-  let selected_token_multiplier: (ReadSignal<f64>, WriteSignal<f64>) = create_signal(0.08);
+  let (from_amount, set_from_amount) = create_signal(0);
+  let (to_amount, set_to_amount) = create_signal(0);
+  let (selected_token, set_selected_token) = create_signal(None);
+  let (selected_token_wallet_balance, set_selected_token_wallet_balance) = create_signal(0);
+  let (selected_token_multiplier, set_selected_token_multiplier) = create_signal(0.08);
   
-  let is_amount_invalid = create_signal(false);
+  let (is_amount_invalid, set_is_amount_invalid) = create_signal(false);
 
-  let request_swap = create_action(move |_| {
-    println!("Request swap");
-  });
+  let request_swap = move || {
+    // Create swap request
+  };
 
-  let show_select_coin_popup = create_signal(false);
-  let show_confirm_popup = create_signal(false);
+  let (show_select_coin_popup, set_show_select_coin_popup) = create_signal(false);
+  let (show_confirm_popup, set_show_confirm_popup) = create_signal(false);
   
     view! {
-        {if (show_confirm_popup.get()) {
-            view! { <ConfirmPopup /> }
+        {move || {
+            if show_confirm_popup.get() {
+                view! { <><ConfirmPopup /></> }
+            } else {
+                view! { <><div class="invisible" /></> }
+            }
         }}
 
-        {if (show_select_coin_popup.get()) {
-            view! { <SelectCoinPopup /> }
+        {move || {
+          if show_select_coin_popup.get() {
+                view! { <><SelectCoinPopup /></> }
+            } else {
+                view! { <><div class="invisible" /></> }
+            }
         }}
 
         <div class="h-screen w-screen block text-white bg-black font-kumbh">
@@ -66,13 +82,13 @@ pub fn SwapPage() -> impl IntoView {
                             <input
                                 type="number"
                                 on:input=move |e| {
-                                    from_amount.set(e.value().parse::<f64>().unwrap_or(0.0));
+                                    set_from_amount(event_target_value(&e).parse::<i32>().unwrap_or(0));
                                 }
                                 placeholder="0"
                                 class="focus:outline-none font-bold text-3xl mr-28 py-2 bg-transparent placeholder:text-zinc-500 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                             />
                             <div class="text-sm flex items-center justify-end gap-2 font-mono text-zinc-300">
-                                <WalletIcon class="w-4 h-4" />
+                                <WalletIcon classes="w-4 h-4".to_string() />
                                 <div>{wallet_balance.get()}</div>
                             </div>
                             <div class="absolute inset-x-0 -bottom-8 z-[4] flex items-center justify-center p-2">
@@ -97,7 +113,7 @@ pub fn SwapPage() -> impl IntoView {
                             <input
                                 type="number"
                                 on:input=move |e| {
-                                    to_amount.set(e.value().parse::<f64>().unwrap_or(0.0));
+                                    set_to_amount(event_target_value(&e).parse::<i32>().unwrap_or(0));
                                 }
                                 value=move || to_amount.get()
                                 placeholder="0"
@@ -116,7 +132,9 @@ pub fn SwapPage() -> impl IntoView {
 
                             <div class="absolute inset-y-0 right-2 z-[5] flex flex-col items-center justify-center">
                                 <button
-                                    on:click=move || show_select_coin_popup.set(true)
+                                    on:click=move |_| {
+                                      set_show_select_coin_popup(true)
+                                    }
                                     class=format!(
                                         "border border-zinc-800 px-3 py-2 flex items-center gap-2 text-sm font-bold {}",
                                         if selected_token.get().is_none() {
@@ -128,6 +146,7 @@ pub fn SwapPage() -> impl IntoView {
                                 >
                                     {if (selected_token.get().is_some()) {
                                         view! {
+                                          <>
                                             <img
                                                 src=selected_token.get().unwrap().icon
                                                 alt=selected_token.get().unwrap().name.clone()
@@ -135,9 +154,14 @@ pub fn SwapPage() -> impl IntoView {
                                             />
                                             <span>{selected_token.get().unwrap().name.clone()}</span>
                                             <ChevronRightIcon classes="w-4 h-4 rotate-90".to_string() />
+                                            </>
                                         }
                                     } else {
-                                        view! { <span>Select token</span> }
+                                        view! { 
+                                          <>
+                                            <span>Select token</span>
+                                          </>
+                                        }
                                     }}
                                 </button>
                             </div>
@@ -149,31 +173,29 @@ pub fn SwapPage() -> impl IntoView {
                             } else {
                                 view! {
                                     <div>
-                                        "1" {swap_from.name.clone()} "="
-                                        {selected_token_multiplier.get()}
-                                        {selected_token.get().unwrap().name.clone()}
+                                        {format!("1 {} = {} {}", swap_from.name.clone(), selected_token_multiplier.get(), selected_token.get().unwrap().name.clone())}
                                     </div>
                                 }
                             }}
                             <div>
-                                "1" {swap_from.name.clone()} "="
-                                {(to_amount.get() / selected_token_multiplier.get()).to_string()}
-                                {selected_token.get().unwrap().name.clone()}
+                                {format!("1 {} = {} {}", swap_from.name.clone(), selected_token_multiplier.get(), selected_token.get().unwrap().name.clone())}
                             </div>
                         </div>
 
                         <Button
-                            on:click=move || request_swap.dispatch(())
-                            disabled=!selected_token.get().is_some() || !from_amount.get()
+                            on_click=move || {
+                              request_swap()
+                            }
+                            disabled=(!selected_token.get().is_some() || !from_amount.get().is_positive())
                         >
-                            0.0
-                            >
                             "Request swap"
                         </Button>
                     </div>
                 </div>
             </div>
         </div>
+
+        
     }
 }
 
@@ -194,11 +216,18 @@ pub fn Header() -> impl IntoView {
 #[component]
 pub fn SelectCoinPopup() -> impl IntoView {
   let target = NodeRef::<Div>::new();
-  on_click_outside(target, move |event| { log!("{:?}", event); });
+  on_click_outside(target, move |event| {
+    // Close popup
+  });
 
-  let coin_search_query = create_signal("".to_string());
-
-  let list_of_coins = create_signal(vec![]);
+  let (coin_search_query, set_coin_search_query) = create_signal("".to_string());
+  let (list_of_coins, set_list_of_coins) = create_signal(vec![
+    SwapToken {
+      name: "Hot".to_string(),
+      icon: "https://picsum.photos/200".to_string(),
+      symbol: "HOT".to_string(),
+    }
+  ]);
 
     view! {
         <div class="fixed z-[999] inset-0">
@@ -211,7 +240,7 @@ pub fn SelectCoinPopup() -> impl IntoView {
                         <input
                             value=move || coin_search_query.get()
                             on:input=move |e| {
-                                coin_search_query.set(e.value().to_string());
+                                set_coin_search_query(event_target_value(&e).to_string());
                             }
                             type="text"
                             placeholder="Search for a token"
@@ -224,11 +253,12 @@ pub fn SelectCoinPopup() -> impl IntoView {
                             .iter()
                             .map(|coin| {
                                 view! {
+                                  <>
                                     <button
                                         class="p-4 flex items-center gap-2 border-zinc-900 hover:bg-zinc-900 active:bg-zinc-800 border-t"
-                                        on:click=move || {
-                                            selected_token.set(Some(coin.clone()));
-                                            show_select_coin_popup.set(false);
+                                        on:click=move |_| {
+                                            //selected_token.set(Some(coin.clone()));
+                                            //show_select_coin_popup.set(false);
                                         }
                                     >
                                         <div class="w-5 h-5 bg-zinc-700 rounded-full overflow-hidden">
@@ -242,10 +272,15 @@ pub fn SelectCoinPopup() -> impl IntoView {
                                         <div class="font-bold">{coin.name.clone()}</div>
                                         <div class="text-zinc-400">{coin.symbol.clone()}</div>
                                     </button>
+                                    </>
                                 }
                             })
                     } else {
-                        view! { <div class="text-zinc-500 text-center py-4">No results found</div> }
+                        view! { 
+                          <>
+                            <div class="text-zinc-500 text-center py-4">No results found</div> 
+                          </>
+                        }
                     }}
                 </div>
             </div>
@@ -256,10 +291,20 @@ pub fn SelectCoinPopup() -> impl IntoView {
 #[component]
 fn ConfirmPopup() -> impl IntoView {
   let target = NodeRef::<Div>::new();
-  on_click_outside(target, move |event| { log!("{:?}", event); });
+  on_click_outside(target, move |event| {
+    // Close popup
+  });
 
-  let from_token = create_signal(None);
-  let to_token = create_signal(None);
+  let (from_token, set_from_token) = create_signal(SwapToken {
+    name: "Hot".to_string(),
+    icon: "https://picsum.photos/200".to_string(),
+    symbol: "HOT".to_string(),
+  });
+  let (to_token, set_to_token) = create_signal(SwapToken {
+    name: "Rock".to_string(),
+    icon: "https://picsum.photos/200".to_string(),
+    symbol: "ROCK".to_string(),
+  });
 
   view! {
       <div class="fixed z-[999] inset-0">
@@ -269,27 +314,28 @@ fn ConfirmPopup() -> impl IntoView {
           >
               <div class="bg-[#191919] relative flex flex-col gap-1 text-white items-center shadow-md rounded-sm p-8">
                   <button
-                      on:click=move || show_confirm_popup.set(false)
+                      on:click=move |_| {
+                        // show_confirm_popup.set(false)
+                      }
                       class="absolute top-3 right-3 z-[999] p-2 bg-[#3A3A3A] rounded-full"
                   >
                       <CloseIcon classes="w-3 h-3".to_string() />
                   </button>
                   <div class="flex items-center gap-4 pt-8">
                       <img
-                          src=from_token.get().unwrap().icon
-                          alt=from_token.get().unwrap().name
+                          src=from_token.get().icon.clone()
+                          alt=from_token.get().name.clone()
                           class="w-20 h-20 rounded-full"
                       />
-                      <LongArrowRightIcon classes="w-8 text-zinc-500".to_string() />
+                      <ArrowRightLongIcon classes="w-8 text-zinc-500".to_string() />
                       <img
-                          src=to_token.get().unwrap().icon
-                          alt=to_token.get().unwrap().name
+                          src=to_token.get().icon.clone()
+                          alt=to_token.get().name.clone()
                           class="w-20 h-20 rounded-full"
                       />
                   </div>
                   <div class="text-xl font-bold text-center pt-3">
-                      {from_token.get().unwrap().name.clone()}to
-                      {to_token.get().unwrap().name.clone()}
+                      {format!("{} to {}", from_token.get().name, to_token.get().name)}
                   </div>
                   <div class="text-xl font-bold text-center text-[#FFC33A]">
                       "Swap request sent successfully!"
