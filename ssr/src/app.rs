f656c96309621cd27c565d70e459348a50071c87
joel-medicala-yral <joel@gobazzinga.io>
2024-12-08 22:19:12 +0530

    lint

--- ssr/src/app.rs
+++ ssr/src/app.rs
@@ -1,7 +1,6 @@
 use crate::page::icpump::ai::ICPumpAi;
 use crate::page::icpump::ICPumpLanding;
 
-use crate::page::wallet::airdrop::AirdropPage;
 // use crate::page::wallet::TestIndex;
 use crate::{
     component::{base_route::BaseRoute, nav::NavBar},
@@ -168,7 +167,6 @@ pub fn App() -> impl IntoView {
                         <Route path="/token/transfer/:token_root" view=TokenTransfer/>
                         <Route path="/board" view=ICPumpLanding/>
                         <Route path="/icpump-ai" view=ICPumpAi/>
-                        // <Route path="/test" view=AirdropPage/>
                     // <Route path="/test" view=TestIndex/>
                     </Route>
                 </Routes>
