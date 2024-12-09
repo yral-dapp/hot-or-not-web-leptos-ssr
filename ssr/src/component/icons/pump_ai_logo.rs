6eb35d6c5d030ed35495138cddda6058ef142546
harsh-mn-yral <harsh@gobazzinga.io>
2024-12-09 15:36:04 +0530

    icons

---
+++ ssr/src/component/icons/pump_ai_logo.rs
@@ -0,0 +1,76 @@
+use leptos::*;
+
+#[component]
+pub fn PumpAILogo(
+    #[prop(optional, default = "w-full h-full".to_string())] classes: String,
+    #[prop(optional)] animate: bool,
+) -> impl IntoView {
+    view! {
+        <svg
+            class=format!("{}", classes)
+            viewBox="0 0 72 72"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M59.3831 43.2173C42.6979 48.7298 37.1418 54.287 31.6292 70.9712C31.1757 72.3429 29.2361 72.3429 28.7826 70.9712C23.2702 54.286 17.713 48.73 1.02874 43.2173C-0.342912 42.7638 -0.342912 40.8242 1.02874 40.3708C17.7139 34.8583 23.27 29.3011 28.7826 12.6169C29.2362 11.2452 31.1757 11.2452 31.6292 12.6169C37.1417 29.3021 42.6989 34.8581 59.3831 40.3708C60.7547 40.8243 60.7547 42.7639 59.3831 43.2173Z"
+                fill="url(#paint0_linear_234_3401)"
+                style=""
+            />
+            <path
+                class=("animate-scale-rotate", animate)
+                d="M71.4854 15.8165C63.1448 18.5709 60.3647 21.3511 57.6065 29.6953C57.3816 30.3812 56.4119 30.3812 56.1833 29.6953C53.4288 21.3547 50.6486 18.5747 42.3044 15.8165C41.6186 15.5916 41.6186 14.6218 42.3044 14.3932C50.645 11.6388 53.425 8.85857 56.1833 0.514375C56.4082 -0.171458 57.3779 -0.171458 57.6065 0.514375C60.361 8.85499 63.1412 11.635 71.4854 14.3932C72.1712 14.6181 72.1712 15.5879 71.4854 15.8165Z"
+                fill="url(#paint1_linear_234_3401)"
+                style=""
+            />
+            <defs>
+                <linearGradient
+                    id="paint0_linear_234_3401"
+                    x1="57.0557"
+                    y1="19.5591"
+                    x2="20.9765"
+                    y2="65.2877"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop
+                        stop-color="#FF78C1"
+                        style="stop-color:#FF78C1;stop-color:color(display-p3 1.0000 0.4701 0.7574);stop-opacity:1;"
+                    />
+                    <stop
+                        offset="0.509385"
+                        stop-color="#E2017B"
+                        style="stop-color:#E2017B;stop-color:color(display-p3 0.8863 0.0039 0.4824);stop-opacity:1;"
+                    />
+                    <stop
+                        offset="1"
+                        stop-color="#5F0938"
+                        style="stop-color:#5F0938;stop-color:color(display-p3 0.3734 0.0335 0.2178);stop-opacity:1;"
+                    />
+                </linearGradient>
+                <linearGradient
+                    id="paint1_linear_234_3401"
+                    x1="70.3215"
+                    y1="3.986"
+                    x2="52.2796"
+                    y2="26.8531"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop
+                        stop-color="#FF78C1"
+                        style="stop-color:#FF78C1;stop-color:color(display-p3 1.0000 0.4701 0.7574);stop-opacity:1;"
+                    />
+                    <stop
+                        offset="0.509385"
+                        stop-color="#E2017B"
+                        style="stop-color:#E2017B;stop-color:color(display-p3 0.8863 0.0039 0.4824);stop-opacity:1;"
+                    />
+                    <stop
+                        offset="1"
+                        stop-color="#5F0938"
+                        style="stop-color:#5F0938;stop-color:color(display-p3 0.3734 0.0335 0.2178);stop-opacity:1;"
+                    />
+                </linearGradient>
+            </defs>
+        </svg>
+    }
+}
