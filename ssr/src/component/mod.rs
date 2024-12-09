78a843f102bfc6d76f04d3e4833cbac0bf6e2aea
harsh-mn-yral <harsh@gobazzinga.io>
2024-12-09 15:28:01 +0530

    lint

--- ssr/src/component/mod.rs
+++ ssr/src/component/mod.rs
@@ -1,9 +1,9 @@
 pub mod airdrop_logo;
 pub mod auth_providers;
 pub mod back_btn;
-pub mod buttons;
 pub mod base_route;
 pub mod bullet_loader;
+pub mod buttons;
 pub mod canisters_prov;
 pub mod coming_soon;
 pub mod connect;
