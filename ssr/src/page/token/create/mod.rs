4a8c2fcfd40cc0e88e8507fb2eb3b3f0854d203a
tushar-saxena-yral <tushar@gobazzinga.io>
2024-12-12 18:42:55 +0530

    feat(token): implement new coming soon banner

--- ssr/src/page/token/create/mod.rs
+++ ssr/src/page/token/create/mod.rs
@@ -456,6 +456,19 @@ pub fn CreateToken() -> impl IntoView {
 }
 
 #[component]
+fn CommingSoonBanner() -> impl IntoView {
+    view! {
+        <div class="with-gradient relative bg-black rounded-md p-4 bg-no-repeat bg-cover bg-left" style="background-image: url('/img/gradient-backdrop.png')">
+            <div class="flex flex-col gap-1">
+                <h2 class="text-white font-bold">Want to raise ICP?</h2>
+                <span class="text-font bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF80] to-[#882B5E80]">Coming Soon...</span>
+            </div>
+            <img src="/img/coming-soon-art.svg" class="absolute right-0 bottom-0" />
+        </div>
+    }
+}
+
+#[component]
 pub fn CreateTokenSettings() -> impl IntoView {
     let fallback_url = "/token/create";
     let ctx: CreateTokenCtx = use_context().unwrap_or_else(|| {
@@ -527,21 +540,7 @@ pub fn CreateTokenSettings() -> impl IntoView {
                     </a>
                 </div>
             </Title>
-            <label class="flex flex-cols-2 cursor-pointer px-1">
-                <span class="flex-1 text-sm font-medium text-gray-400 dark:text-gray-500">
-                    Do you want to raise ICP?
-                </span>
-                <div>
-                    <span class="text-sm font-medium text-gray-400 dark:text-gray-500">
-                        Coming Soon!
-                    </span>
-                // <input type="checkbox" value="" class="sr-only peer" checked disabled />
-                // <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:bg-gray-600">
-                // <div class="absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition-transform peer-checked:translate-x-5 dark:border-gray-600"/>
-                // </div>
-                </div>
-            // <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-600"></div>
-            </label>
+            <CommingSoonBanner />
             <form _ref=form_ref>
                 <InputBox
                     disabled=true
