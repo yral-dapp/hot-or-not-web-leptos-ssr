46155d4e07b776adb238f6844349d4372e4935d8
tushar-saxena-yral <tushar@gobazzinga.io>
2025-01-05 05:44:26 +0530

    feat: put games count and wallet balance behind a lazily loaded signal

--- ssr/src/page/icpump/pumpndump.rs
+++ ssr/src/page/icpump/pumpndump.rs
@@ -1,15 +1,16 @@
-use leptos::{component, view, IntoView};
+use leptos::{component, expect_context, provide_context, view, IntoView, Resource, SignalGet};
 use leptos_icons::Icon;
 
 #[component]
-fn Header(#[prop()] wallet_balance: u64, #[prop()] games_count: u64) -> impl IntoView {
+fn Header() -> impl IntoView {
+    let data = expect_context::<Resource<(), PlayerGamesCountAndBalance>>();
     view! {
         <div class="flex items-center w-full justify-between py-2 gap-8">
             <a
                 href="/pump-dump/profile"
                 class="flex flex-col text-right text-sm ml-8 relative bg-[#171717] rounded-lg pt-1 pb-1.5 pr-3 pl-8"
             >
-                <div class="font-bold text-sm">{games_count}</div>
+                <div class="font-bold text-sm">{move || data.get().map(|d| format!("{}", d.games_count)).unwrap_or_else(|| "----".into())}</div>
                 <div class="text-xs text-[#A3A3A3] uppercase">Games</div>
                 <img
                     src="/img/gamepad.png"
@@ -23,7 +24,7 @@ fn Header(#[prop()] wallet_balance: u64, #[prop()] games_count: u64) -> impl Int
                 <div
                     class="font-bold absolute top-1 text-sm"
                 >
-                    {wallet_balance}
+                    {move || data.get().map(|d| format!("{}", d.wallet_balance)).unwrap_or_else(|| "----".into())}
                 </div>
                 <div class="h-5 opacity-0"></div>
                 <div class="text-xs text-[#A3A3A3]">gDOLR</div>
@@ -40,14 +41,42 @@ fn Header(#[prop()] wallet_balance: u64, #[prop()] games_count: u64) -> impl Int
     }
 }
 
+#[derive(Clone, Copy, Debug, serde::Serialize, serde::Deserialize)]
+struct PlayerGamesCountAndBalance {
+    games_count: u64,
+    wallet_balance: u64,
+}
+
+impl PlayerGamesCountAndBalance {
+    pub fn new(games_count: u64, wallet_balance: u64) -> Self {
+        Self {
+            games_count,
+            wallet_balance,
+        }
+    }
+
+    #[cfg(any(feature = "local-bin", feature = "local-lib"))]
+    pub async fn load() -> Self {
+        Self::new(0, 1000)
+    }
+
+    #[cfg(not(any(feature = "local-bin", feature = "local-lib")))]
+    pub async fn load() -> Self {
+        unimplemented!("Haven't figured out how to load games count and wallet balance yet")
+    }
+}
+
 #[component]
 pub fn PumpNDump() -> impl IntoView {
-    let wallet_balance = 0;
-    let games_count = 0;
+    provide_context(Resource::new(
+        move || (),
+        |_| PlayerGamesCountAndBalance::load(),
+    ));
+
     view! {
         <div class="h-screen w-screen block text-white bg-black">
             <div class="max-w-md flex flex-col relative w-full mx-auto items-center h-full px-4 py-4">
-                <Header wallet_balance games_count />
+                <Header />
             </div>
         </div>
     }
