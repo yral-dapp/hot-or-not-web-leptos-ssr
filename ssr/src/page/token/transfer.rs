40180fce7d87d9f5013013848dac73f380112233
joel-medicala-yral <joel@gobazzinga.io>
2025-01-07 19:27:24 +0530

    Fixed wallet page usdc tokens not loading (#579)
    * init
    
    * fixed infini
    
    * fixed profile page crashes
    
    * e2e
    
    * fixed e2e test
    
    * fixed ck coins not redirecting
    
    * added padding
    
    * usdc
    
    * e2e
    
    * coyns wait
    
    * fixed tests
    
    * nits
    
    * bumped revs

--- ssr/src/page/token/transfer.rs
+++ ssr/src/page/token/transfer.rs
@@ -219,6 +219,7 @@ fn TokenTransferInner(cans: Canisters<true>, root: RootType, info: TokenMetadata
                     )
                     .await?;
                 }
+                RootType::COYNS => return Err(ServerFnError::new("Coyns cannot be transferred")),
             }
             TokensTransferred.send_event(amt.e8s.to_string(), destination, cans.clone());
 
