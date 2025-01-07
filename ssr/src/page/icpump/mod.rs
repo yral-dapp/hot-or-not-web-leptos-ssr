3b4e5cb0ac516f11a4184a8812fb5b68ed2f5af8
joel-medicala-yral <joel@gobazzinga.io>
2025-01-07 19:52:35 +0530

    Fixed limited number of pages showing (#574)
    * nuked filters
    
    * added e2e test
    
    * e2e

--- ssr/src/page/icpump/mod.rs
+++ ssr/src/page/icpump/mod.rs
@@ -53,29 +53,33 @@ async fn process_token_list_item(
                     .trim_end_matches('/')
                     .split('/')
                     .last()
-                    .ok_or(ServerFnError::new("Not root given"))?,
-            )?;
+                    .ok_or(ServerFnError::new("Not root given"))
+                    .unwrap_or_default(),
+            )
+            .unwrap_or(Principal::anonymous());
 
-            let token_owner_canister_id = cans.get_token_owner(root_principal).await?;
+            let token_owner_canister_id = cans
+                .get_token_owner(root_principal)
+                .await
+                .unwrap_or_default();
             let is_airdrop_claimed = if let Some(token_owner) = token_owner_canister_id {
                 cans.get_airdrop_status(token_owner.canister_id, root_principal, key_principal)
-                    .await?
+                    .await
+                    .unwrap_or(true)
             } else {
                 true
             };
             // let token_owner = cans.individual_user(token_owner_canister_id.unwrap()).await;
             // token_owner_principal_id: token_owner.get_profile_details().await.unwrap().principal_id,
-            Ok::<_, ServerFnError>(ProcessedTokenListResponse {
+            ProcessedTokenListResponse {
                 token_details: token,
                 root: root_principal,
                 is_airdrop_claimed,
-            })
+            }
         });
     }
 
-    fut.filter_map(|result| async move { result.ok() })
-        .collect()
-        .await
+    fut.collect().await
 }
 #[component]
 pub fn ICPumpListing() -> impl IntoView {
