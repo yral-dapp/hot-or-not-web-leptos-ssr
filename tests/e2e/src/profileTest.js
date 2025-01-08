9cc11beac9b7a14e6a81024780672b1e39913874
ravi-sawlani-yral <ravi@gobazzinga.io>
2025-01-08 09:38:45 +0530

    fix timeouts in test

--- tests/e2e/src/profileTest.js
+++ tests/e2e/src/profileTest.js
@@ -9,7 +9,7 @@ describe('Profile page tests', function () {
 
 
     it('profile page', async function (browser) {
-        browser.element.findByText('Login').waitUntil('visible', { timeout: 500000 }).click()
+        browser.element.findByText('Login', {timeout: 10000}).waitUntil('visible').click()
         browser.element.findByText('Google Sign-In').waitUntil('visible')
         browser.percySnapshot('SignIn Modal')
     })
--- tests/e2e/src/walletTest.js
+++ tests/e2e/src/walletTest.js
@@ -4,14 +4,12 @@ describe("wallet page tests", function () {
     })
 
     it("wallet page contains login button", async function (browser) {
-        browser.element.findByText('Login to claim your COYNs').waitUntil('enabled', {
-            timeout: 10000
-        });
+        browser.element.findByText('Login to claim your COYNs', {timeout: 10000}).waitUntil('enabled');
     })
     
     it("default wallet page contains 1000 COYNS", async function(browser){
-        browser.element.findByText("COYNS", {timeout: 10000}).waitUntil('enabled', {timeout: 10000})
-        browser.element.findByText("1000", {timeout: 10000}).waitUntil('enabled', {timeout: 10000});
+        browser.element.findByText("COYNS", {timeout: 10000}).waitUntil('enabled')
+        browser.element.findByText("1000", {timeout: 10000}).waitUntil('enabled');
     })
     it("default wallet page contains 1000 COYNS", function(browser) {
         browser.waitForElementVisible('body', 10000);
@@ -19,8 +17,8 @@ describe("wallet page tests", function () {
         browser.pause(10000);
     
     
-        browser.element.findByText("COYNS").waitUntil('visible', { timeout: 10000 }).assert.enabled();
-        browser.element.findByText("1000").waitUntil('visible', { timeout: 10000 }).assert.enabled();
+        browser.element.findByText("COYNS", { timeout: 10000 }).waitUntil('visible').assert.enabled();
+        browser.element.findByText("1000", { timeout: 10000 }).waitUntil('visible').assert.enabled();
     
     });
 
@@ -32,6 +30,6 @@ describe("wallet page tests", function () {
 
     it("check usdc  loading", async function (browser){
         browser.url(`${browser.launchUrl}/wallet/34yzw-zrmgu-vg6ms-2uj2a-czql2-7y4bu-mt5so-ckrtz-znelw-yyvr4-2ae`);
-        browser.element.findByText('USDC', {timeout: 15000}).waitUntil('visible', { timeout: 10000 }).assert.enabled()
+        browser.element.findByText('USDC', {timeout: 20000}).waitUntil('visible', { timeout: 10000 }).assert.enabled()
     })
 })
