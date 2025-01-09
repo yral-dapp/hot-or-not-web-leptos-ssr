c9a466d8768770f6a8ec8de6a39ff7b0367b6d95
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2025-01-09 10:53:16 +0530

    [skip ci] feat: Integrate percy (#588)
    * integrate percy
    
    * empty commit
    
    * add commit reference as buildName for e2e test workflow
    
    * fix timeouts in test
    
    * added percy token as github secret

--- tests/e2e/src/walletTest.js
+++ tests/e2e/src/walletTest.js
@@ -4,7 +4,12 @@ describe("wallet page tests", function () {
     })
 
     it("wallet page contains login button", async function (browser) {
-        browser.element.findByText('Login to claim your COYNs').assert.enabled()
+        browser.element.findByText('Login to claim your COYNs', {timeout: 10000}).waitUntil('enabled');
+    })
+    
+    it("default wallet page contains 1000 COYNS", async function(browser){
+        browser.element.findByText("COYNS", {timeout: 10000}).waitUntil('enabled')
+        browser.element.findByText("1000", {timeout: 10000}).waitUntil('enabled');
     })
     it("default wallet page contains 1000 COYNS", function(browser) {
         browser.waitForElementVisible('body', 10000);
@@ -12,18 +17,19 @@ describe("wallet page tests", function () {
         browser.pause(10000);
     
     
-        browser.element.findByText("COYNS").waitUntil('visible', { timeout: 10000 }).assert.enabled();
-        browser.element.findByText("1000").waitUntil('visible', { timeout: 10000 }).assert.enabled();
+        browser.element.findByText("COYNS", { timeout: 10000 }).waitUntil('visible').assert.enabled();
+        browser.element.findByText("1000", { timeout: 10000 }).waitUntil('visible').assert.enabled();
     
     });
 
+    it('wallet page snapshot test', function(browser) {
+        browser.percySnapshot('Wallet Page')
+
+    })
+
+
     it("check usdc  loading", async function (browser){
         browser.url(`${browser.launchUrl}/wallet/34yzw-zrmgu-vg6ms-2uj2a-czql2-7y4bu-mt5so-ckrtz-znelw-yyvr4-2ae`);
-        
-        browser.waitForElementVisible('body', 10000);
-    
-        browser.pause(10000);
-
-        browser.element.findByText('USDC').waitUntil('visible', { timeout: 10000 }).assert.enabled()
+        browser.element.findByText('USDC', {timeout: 20000}).waitUntil('visible', { timeout: 10000 }).assert.enabled()
     })
 })
