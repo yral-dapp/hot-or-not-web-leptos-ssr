66e08b5ea8d3b4563183ff17d85904755cdcbe21
vishnu <vishnu@gobazzinga.io>
2025-01-10 15:19:58 +0530

    Increase page size

--- ssr/src/consts/mod.rs
+++ ssr/src/consts/mod.rs
@@ -43,7 +43,7 @@ pub static FALLBACK_USER_INDEX: Lazy<Principal> =
 
 pub const ICP_LEDGER_CANISTER_ID: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 
-pub const ICPUMP_LISTING_PAGE_SIZE: usize = 24;
+pub const ICPUMP_LISTING_PAGE_SIZE: usize = 48;
 
 pub const CDAO_SWAP_PRE_READY_TIME_SECS: u64 = 150;
 
