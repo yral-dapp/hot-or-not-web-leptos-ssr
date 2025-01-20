3b96b429ccd5f3efef4243cdd4aff8372a305038
komal-rs <komalsai@gobazzinga.io>
2025-01-20 19:55:31 +0530

    const

--- ssr/src/consts/mod.rs
+++ ssr/src/consts/mod.rs
@@ -29,9 +29,9 @@ pub const USER_PRINCIPAL_STORE: &str = "user-principal";
 pub const USER_ONBOARDING_STORE: &str = "user-onboarding";
 
 pub static OFF_CHAIN_AGENT_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("https://pr-118-yral-dapp-off-chain-agent.fly.dev/").unwrap());
+    Lazy::new(|| Url::parse("https://pr-120-yral-dapp-off-chain-agent.fly.dev/").unwrap());
 pub static OFF_CHAIN_AGENT_GRPC_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("https://pr-118-yral-dapp-off-chain-agent.fly.dev:443").unwrap()); // pr-91-yral-dapp-off-chain-agent
+    Lazy::new(|| Url::parse("https://pr-120-yral-dapp-off-chain-agent.fly.dev:443").unwrap()); // pr-91-yral-dapp-off-chain-agent
                                                                                                // G-6W5Q2MRX0E to test locally | G-PLNNETMSLM
 pub static GTAG_MEASUREMENT_ID: Lazy<&str> = Lazy::new(|| "G-PLNNETMSLM");
 pub static DOWNLOAD_UPLOAD_SERVICE: Lazy<Url> =
