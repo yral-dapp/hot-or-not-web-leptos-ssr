d89ebdc8b7490a6c9c5dc9b922050ca256841975
tushar-saxena-yral <tushar@gobazzinga.io>
2025-02-12 20:43:57 +0530

    chore: use tailwind color instead of hard coding them

--- ssr/src/component/tooltip.rs
+++ ssr/src/component/tooltip.rs
@@ -14,8 +14,8 @@ pub fn Tooltip(
             <div class="tooltip-target bg-neutral-800 grid place-items-center size-[22px] rounded-full cursor-pointer">
                 <Icon class="size-[22px]" icon=icon />
             </div>
-            <div class="w-max max-w-[65vw] md:max-w-[400px] absolute pointer-events-none duration-150 rounded-md top-0 mt-8 z-50 opacity-0 group-hover:opacity-100 bg-[#EAC9DB] text-[#A00157] p-4 left-1/2 -translate-x-1/2 ml-2">
-                <div class="absolute mr-1 right-1/2 bottom-full h-0 w-0 border-r-4 border-l-4 border-b-4 border-l-transparent border-r-transparent border-b-[#EAC9DB]"></div>
+            <div class="w-max max-w-[65vw] md:max-w-[400px] absolute pointer-events-none duration-150 rounded-md top-0 mt-8 z-50 opacity-0 group-hover:opacity-100 bg-primary-200 text-primary-800 p-4 left-1/2 -translate-x-1/2 ml-2">
+                <div class="absolute mr-1 right-1/2 bottom-full h-0 w-0 border-r-4 border-l-4 border-b-4 border-l-transparent border-r-transparent border-b-primary-200"></div>
                 <h2 class="font-bold">{title}</h2>
                 <div>
                     {description}
