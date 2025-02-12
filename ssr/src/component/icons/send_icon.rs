bf63c6b77cfc1a73c8c3c7e01d3e72d310e7a4c0
harsh-mn-yral <harsh@gobazzinga.io>
2025-02-12 11:13:28 +0530

    fix

--- ssr/src/component/icons/send_icon.rs
+++ ssr/src/component/icons/send_icon.rs
@@ -7,21 +7,13 @@ pub fn SendIcon(
 ) -> impl IntoView {
     if filled {
         view! {
-            <svg  class=format!("text-neutral-200 {}", class) viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_335_556)">
+            <svg class=format!("text-neutral-200 {}", class) viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7.83362 10.2583L11.5459 16.0919L16.3189 1.77297L1.99999 6.54594L7.83362 10.2583ZM7.83362 10.2583L12.0763 6.01561" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                </g>
-                <defs>
-                    <clipPath id="clip0_335_556">
-                        <rect width="18" height="18" fill="white" transform="translate(0.5)"/>
-                    </clipPath>
-                </defs>
             </svg>
-
         }
     } else {
         view! {
-            <svg class=format!("text-neutral-200 {}", class) viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg  class=format!("text-neutral-200 {}", class) viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clip-path="url(#clip0_335_556)">
                     <path d="M7.83362 10.2583L11.5459 16.0919L16.3189 1.77297L1.99999 6.54594L7.83362 10.2583ZM7.83362 10.2583L12.0763 6.01561" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </g>
