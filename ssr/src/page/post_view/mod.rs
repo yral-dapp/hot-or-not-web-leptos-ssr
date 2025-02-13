b56056354a873c77f7f5ed31852636034e9f4799
harsh-mn-yral <harsh@gobazzinga.io>
2025-02-13 17:32:19 +0530

    fmt

--- ssr/src/page/post_view/mod.rs
+++ ssr/src/page/post_view/mod.rs
@@ -10,7 +10,10 @@ use std::cmp::Reverse;
 use crate::{
     component::{scrolling_post_view::ScrollingPostView, spinner::FullScreenSpinner},
     consts::NSFW_TOGGLE_STORE,
-    state::{app_state::AppState, canisters::{authenticated_canisters, unauth_canisters}},
+    state::{
+        app_state::AppState,
+        canisters::{authenticated_canisters, unauth_canisters},
+    },
     try_or_redirect,
     utils::{posts::FetchCursor, route::failure_redirect},
 };
@@ -18,8 +21,8 @@ use candid::Principal;
 use codee::string::FromToStringCodec;
 use futures::StreamExt;
 use leptos::*;
-use leptos_router::*;
 use leptos_meta::*;
+use leptos_router::*;
 use leptos_use::{storage::use_local_storage, use_debounce_fn};
 
 use video_iter::{FeedResultType, VideoFetchStream};
@@ -327,7 +330,7 @@ pub fn PostView() -> impl IntoView {
 
     let app_state = use_context::<AppState>();
     let page_title = app_state.unwrap().name.to_owned() + " - Home";
-    
+
     view! {
         <Title text=page_title />
         <Suspense fallback=FullScreenSpinner>
