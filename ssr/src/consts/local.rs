4041928c27f8bcbe91d70b6d899fceed491103c0
vishnu <vishnu@gobazzinga.io>
2025-02-20 14:04:50 +0530

    Remove local testing changes

--- ssr/src/consts/local.rs
+++ ssr/src/consts/local.rs
@@ -6,7 +6,7 @@ pub static METADATA_API_BASE: Lazy<Url> =
 
 pub const AGENT_URL: &str = "http://localhost:4943";
 
-pub const YRAL_BACKEND_CONTAINER_TAG: &str = "testing-1";
+pub const YRAL_BACKEND_CONTAINER_TAG: &str = "04b53277579d9370c13312a2833ca0b855cdad72";
 pub const YRAL_METADATA_CONTAINER_TAG: &str = "a4879e2e711c17beeb12ed6987ba315c110be9e5";
 pub static PUMP_AND_DUMP_WORKER_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("http://localhost:8787/").unwrap());
