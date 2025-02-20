df88b74cbb000442936be73782606d7bd1510168
harsh-mn-yral <harsh@gobazzinga.io>
2025-02-20 11:58:35 +0530

    Fix report icon

--- ssr/src/component/icons/report_icon.rs
+++ ssr/src/component/icons/report_icon.rs
@@ -3,7 +3,5 @@ use crate::utils::icon::icon_gen;
 icon_gen!(
     ReportIcon,
     view_box = "0 0 36 36",
-    r###"
-      <path d="M18 13.5V16.5M18 22.5H18.015M7.60801 28.5H28.392C30.702 28.5 32.145 25.9995 30.99 24L20.598 5.99999C19.443 4.00049 16.557 4.00049 15.402 5.99999L5.01001 24C3.85501 25.9995 5.29801 28.5 7.60801 28.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    "###
+    r###"<path d="M18 13.5v3m0 6h0m-10.4 6h20.8A3 3 0 0 0 31 24L20.6 6a3 3 0 0 0-5.2 0L5 24a3 3 0 0 0 2.6 4.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>"###
 );
