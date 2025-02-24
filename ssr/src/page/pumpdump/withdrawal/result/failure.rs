842f493d4c15f124f8956251ccc1a0cdf055cca9
Harsha <106158714+harsh-mn-yral@users.noreply.github.com>
2025-02-24 16:59:35 +0530

    Cleanup assets (#692)
    changelog:
    - optimized images 
    - organized images
    - deleted unused assets
    - fixed hot or not manifest
    - fixed screenshot res error

--- ssr/src/page/pumpdump/withdrawal/result/failure.rs
+++ ssr/src/page/pumpdump/withdrawal/result/failure.rs
@@ -17,14 +17,14 @@ pub fn Failure() -> impl IntoView {
     let formatted_cents = TokenBalance::new(cents.clone(), 6).humanize_float_truncate_to_dp(4);
     Some(view! {
         <div
-            style:background-image="url('/img/gradient-grayscale.png')"
+            style:background-image="url('/img/pumpdump/onboarding-bg-grayscale.webp')"
             class="min-h-screen w-full flex flex-col text-white pt-2 pb-12 bg-black items-center relative max-md:bg-[length:271vw_100vh] md:bg-[length:max(100vw,100vh)] max-md:bg-[position:-4.5vw_-6.5vh] md:bg-bottom"
         >
             <div id="back-nav" class="flex flex-col items-center w-full gap-20 pb-16"></div>
             <div class="w-full">
                 <div class="max-w-md w-full mx-auto px-4 mt-4 pb-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <div class="w-full flex flex-col gap-12 items-center">
-                        <img class="max-w-44" src="/img/cross-3d.png" />
+                        <img class="max-w-44" src="/img/pumpdump/cross-3d-grayscale.webp" />
                         <div class="flex flex-col gap-8 w-full px-5">
                             <div class="flex flex-col gap-2 items-center">
                                 <span class="font-bold text-lg">OOPS!</span>
