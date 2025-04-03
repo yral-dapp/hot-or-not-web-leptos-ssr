39b5afa5e37746b0f585b2c7d3058f740eba5199
komal-sai-yral <komalsai@gobazzinga.io>
2025-04-03 19:37:03 +0530

    link

--- ssr/src/consts/src/lib.rs
+++ ssr/src/consts/src/lib.rs
@@ -37,7 +37,7 @@ pub static GTAG_MEASUREMENT_ID: Lazy<&str> = Lazy::new(|| "G-PLNNETMSLM");
 pub static DOWNLOAD_UPLOAD_SERVICE: Lazy<Url> =
     Lazy::new(|| Url::parse("https://download-upload-service.fly.dev").unwrap());
 pub static ML_FEED_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("https://yral-ml-feed-server.fly.dev").unwrap());
+    Lazy::new(|| Url::parse("https://yral-ml-feed-server-staging.fly.dev").unwrap());
 
 pub static FALLBACK_USER_INDEX: Lazy<Principal> =
     Lazy::new(|| Principal::from_text("rimrc-piaaa-aaaao-aaljq-cai").unwrap());
