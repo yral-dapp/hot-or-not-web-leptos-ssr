d8720850434b73ab0ae2c0855c2a05c02a6a6c33
komal-sai-yral <komalsai@gobazzinga.io>
2025-04-11 13:49:31 +0530

    test

--- ssr/src/consts/src/lib.rs
+++ ssr/src/consts/src/lib.rs
@@ -32,7 +32,7 @@ pub static OFF_CHAIN_AGENT_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://pr-181-yral-dapp-off-chain-agent.fly.dev").unwrap());
 pub static OFF_CHAIN_AGENT_GRPC_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://pr-181-yral-dapp-off-chain-agent.fly.dev:443").unwrap()); // pr-91-yral-dapp-off-chain-agent https://icp-off-chain-agent.fly.dev:443
-                                                                                  // G-6W5Q2MRX0E to test locally | G-PLNNETMSLM
+                                                                                               // G-6W5Q2MRX0E to test locally | G-PLNNETMSLM
 pub static GTAG_MEASUREMENT_ID: Lazy<&str> = Lazy::new(|| "G-PLNNETMSLM");
 pub static DOWNLOAD_UPLOAD_SERVICE: Lazy<Url> =
     Lazy::new(|| Url::parse("https://download-upload-service.fly.dev").unwrap());
