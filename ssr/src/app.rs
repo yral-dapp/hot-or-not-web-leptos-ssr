a0974b2f4ca7fcb77254aa39a2e250e8995840f8
harsh-mahajan-yral <harshmahajan@gobazzinga.io>
2025-04-16 22:04:44 +0530

    chore: adding google tag manager analytics

--- ssr/src/app.rs
+++ ssr/src/app.rs
@@ -98,8 +98,22 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <HashedStylesheet id="leptos" options=options.clone()/>
                 <HydrationScripts options/>
                 <MetaTags/>
+                <Script>
+                    {r#"
+                    (function(w,d,s,l,i){
+                        w[l]=w[l]||[];
+                        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                        var f=d.getElementsByTagName(s)[0], 
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                        j.async=true;
+                        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                        f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MNBWSPVJ');
+                    "#}
+                </Script>
             </head>
             <body>
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNBWSPVJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>
                 <App/>
             </body>
         </html>
